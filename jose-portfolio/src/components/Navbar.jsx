d936898bd09b3aed9de42fc3c9a1bff0e87dd304
Rafa2908 <papaus02@gmail.com>
2026-05-05 20:02:22 -0400

    Added media query for responsiveness

--- jose-portfolio/src/components/Navbar.jsx
+++ jose-portfolio/src/components/Navbar.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <div className="navbar-container">
-      <Link to={"/"}>
+      <Link to={"/"} onClick={closeMenu}>
         <h1>
           Jose <span>Portfolio</span>
         </h1>
       </Link>
-      <ul className="navbar-items">
-        <Link to={"/projects"}>
+
+      {/* HAMBURGER BUTTON */}
+      <button
+        className="navbar-hamburger"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Toggle menu"
+      >
+        <span className={`bar ${isOpen ? "open" : ""}`}></span>
+        <span className={`bar ${isOpen ? "open" : ""}`}></span>
+        <span className={`bar ${isOpen ? "open" : ""}`}></span>
+      </button>
+
+      {/* NAV LINKS */}
+      <ul className={`navbar-items ${isOpen ? "navbar-open" : ""}`}>
+        <Link to={"/projects"} onClick={closeMenu}>
           <li>Projects</li>
         </Link>
-        <Link to={"/about"}>
+        <Link to={"/about"} onClick={closeMenu}>
           <li>About</li>
         </Link>
-        <Link to={"/contact"}>
+        <Link to={"/contact"} onClick={closeMenu}>
           <li>Contact</li>
         </Link>
       </ul>
